2d77fbff757ea8a5cd9f5a71f5a08fbc80725886
come-wale-senusi <akinyemisaheedwale@gmail.com>
2026-07-16 10:32:16 +0100

    Configure email using environment variables

--- action_php/database.php
+++ action_php/database.php
@@ -1,17 +1,19 @@
 <?php
    
-    $conn = mysqli_connect('127.0.0.1', 'root', '', 'spring');
     require_once __DIR__ . '/../bootstrap.php';
-
     $conn = mysqli_connect(
-        $_ENV['DB_HOST'],
-        $_ENV['DB_USERNAME'],
-        $_ENV['DB_PASSWORD'],
-        $_ENV['DB_DATABASE']
+    $_ENV['DB_HOST'],
+    $_ENV['DB_USERNAME'],
+    $_ENV['DB_PASSWORD'],
+    $_ENV['DB_DATABASE']
     );
 
     if (!$conn) {
         die('Database connection failed: ' . mysqli_connect_error());
     }
+    
+    // $conn = mysqli_connect('127.0.0.1', 'root', '', 'spring');
+    // $conn = mysqli_connect("sql210.infinityfree.com", 'if0_42356454', 'XtLgtWErK77qOG', 'if0_42356454_sms');
+
 
 ?>
--- util/email_config.util.php
+++ util/email_config.util.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once __DIR__ . '/../bootstrap.php';
 require_once __DIR__ . "/../phpmailer/PHPMailer.php";
 require_once __DIR__ . "/../phpmailer/SMTP.php";
 require_once __DIR__ . "/../phpmailer/Exception.php";

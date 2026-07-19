2c76c2b7bce74e8de37e7672a60a4624d18ecf9d
Yusuf Iysah  | UX Engineer <yusufiysah@gmail.com>
2026-07-19 11:42:05 +0100

    Redesign director authentication forms for improved user experience

--- administration/director/director_forgot_password.php
+++ administration/director/director_forgot_password.php
@@ -1,14 +1,14 @@
 <?php
     session_start();
-    
+
 
     $result = '';
 
     if (isset($_GET['result'])) {
-        
+
         $result = $_GET['result'];
     }
-    
+
 ?>
 
 
@@ -19,24 +19,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>director forgot password</title>
-    <link rel="stylesheet" href="../principal/css/principal_login_css.css">
+    <link rel="stylesheet" href="css/director_login_css.css">
 </head>
 <body>
     <div id="form_container">
         <div class="form_element">
             <form action="action_php/director_forgot_password_action.php" method="POST">
-                <h2>director forgot password form</h2> 
-                <div style="text-align: center; margin-bottom: 20px;">
-                    <span style="color: tomato;"><?php echo $result; ?></span>
-                </div>
-                
+                <h2>forgot password</h2>
+                <p class="form_sub">Enter your email address and we'll send you a code to reset your password.</p>
+
+                <p class="form_msg"><?php echo $result; ?></p>
+
                 <input type="email" id="email" placeholder="Enter Email" required name="email">
 
-                
+                <input type="submit" name="submit" id="reg_btn" value="send reset code">
 
-                <input type="submit" name="submit" id="reg_btn" value="submit">
-
-                
+                <div id="forgot">
+                    <a href="director_login.php">back to login</a>
+                </div>
             </form>
         </div>
     </div>
--- administration/director/director_password_reset.php
+++ administration/director/director_password_reset.php
@@ -1,13 +1,13 @@
 <?php
     session_start();
-    
+
 
     if (isset($_GET['token'])) {
-        
+
        $token = $_GET['token'];
-       
+
     }
-    
+
 ?>
 
 
@@ -18,33 +18,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>director reset password</title>
-    <link rel="stylesheet" href="../principal/css/principal_login_css.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="../../../javascript/jquery.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/director_login_css.css">
+    <script src="../../javascript/jquery.js"></script>
 </head>
-<body style="background-color: #eef0ef;">
+<body>
     <div id="form_container">
         <div class="form_element">
-            <form  method="POST" id="form">
-                <h2> director reset password form</h2>
-                
-                <div style="text-align: center; margin-bottom: 20px;">
-                    <p id="error" style="color: tomato;"></p>
-                </div>
-                <input type="text" id="email" placeholder="Enter code" required name="code">
+            <form method="POST" id="form">
+                <h2>reset password</h2>
+                <p class="form_sub">Enter the code sent to your email and choose a new password.</p>
+
+                <p class="form_msg" id="error"></p>
+
+                <input type="text" id="code" placeholder="Enter Code" required name="code">
 
                 <input type="password" id="pwd" class="pwd" placeholder="New Password" required name="password">
 
-                <input type="password" id="pwd" class="pwd_confirm" placeholder="Confirm Password" required name="user_name">
+                <input type="password" id="pwd_confirm" class="pwd_confirm" placeholder="Confirm Password" required name="user_name">
 
                 <input type="hidden" id="token" name="token" value="<?php echo $token ?>">
 
-                <input type="submit" name="submit" id="reg_btn" value="submit">
+                <input type="submit" name="submit" id="reg_btn" value="reset password">
 
-                
+                <div id="forgot">
+                    <a href="director_login.php">back to login</a>
+                </div>
             </form>
         </div>
     </div>
@@ -57,13 +55,13 @@
 
                 event.preventDefault();
 
-                var code = $('#email').val();
+                var code = $('#code').val();
                 var pwd = $('.pwd').val();
                 var confirm_pwd = $('.pwd_confirm').val()
                 var token = $('#token').val();
-                
+
                 if (code == '' || pwd == '' || confirm_pwd == '') {
-                    
+
                     $('#error').text('please fill all space provided......');
                     $('#form')[0].reset();
 
@@ -86,11 +84,11 @@
                                 $('#error').text('');
 
                             }, 7000);
-                        
+
                     }else{
 
                         if (pwd.length < 8) {
-                            
+
 
                             $('#error').text('password must be atleast 8 characters.....');
                             $('#form')[0].reset();
@@ -116,11 +114,11 @@
 
                                 success: function(data){
 
-                                    $('#reg_btn').val('Submit');
+                                    $('#reg_btn').val('reset password');
                                     $('#reg_btn').attr('disabled', false);
 
                                     if (data == 'updated') {
-                                        
+
                                         window.location.assign("director_login.php?result='reset_pwd'");
 
                                     }else{
@@ -154,7 +152,6 @@
 
 
 
-            
         })
 
 

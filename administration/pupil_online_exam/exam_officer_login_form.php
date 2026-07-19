bf950f91dd185b3f6486660e9d0560be7acaf5c9
Yusuf Iysah  | UX Engineer <yusufiysah@gmail.com>
2026-07-19 13:53:10 +0100

    Redesign login forms for exam officers and super admins with improved layout, unified CSS, and enhanced user feedback

--- administration/pupil_online_exam/exam_officer_login_form.php
+++ administration/pupil_online_exam/exam_officer_login_form.php
@@ -8,30 +8,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>e-exam login form</title>
-    <link rel="stylesheet" href="css/school_name_verification_css.css">
+    <link rel="stylesheet" href="../css/auth_css.css?v=1">
     <script src="../../javascript/jquery.js"></script>
 </head>
 <body>
-    <div id="form_container">
-        <div class="form_element">
+    <main class="page">
+        <div class="card">
+            <div class="icon_badge">
+                <img src="../../image/school/logo.jpg" alt="Acadex logo">
+            </div>
+
+            <h1>E-exam access</h1>
+            <p class="sub">Enter the exam officer credentials to open the pupil exam portal.</p>
+
+            <p class="js_alert" id="error"></p>
+
             <form action="" id="form">
-
-                <div class="error">
-                    <p id="error"></p>
+                <div class="field">
+                    <label for="email">Email address</label>
+                    <input type="email" id="email" class="email" placeholder="you@example.com" required name="email">
                 </div>
-                <h2>E-exam login form for pupils</h2>
-                <input type="email" id="pwd" class="email" placeholder="Email" required name="email">
-          
-                <input type="text" id="pwd" class="user" placeholder="User Name" required name="user_name">
 
-                <input type="password" id="pwd" class="pwd" placeholder="Enter Password" required name="password">
+                <div class="field">
+                    <label for="user">User name</label>
+                    <input type="text" id="user" class="user" placeholder="Enter your user name" required name="user_name">
+                </div>
 
-                <input type="submit" name="submit" id="reg_btn" value="verify">
+                <div class="field">
+                    <label for="pwd">Password</label>
+                    <input type="password" id="pwd" class="pwd" placeholder="Enter your password" required name="password">
+                </div>
 
-                
+                <input type="submit" name="submit" id="reg_btn" class="submit_btn" value="Verify">
             </form>
         </div>
-    </div>
+
+        <p class="page_footer">Acadex &middot; by Zorfts Technologies Ltd</p>
+    </main>
 
 
 
@@ -53,23 +66,23 @@
 
             }
 
-            
 
 
-            
+
+
             // submiting admin officer for login..................
 
             $('#reg_btn').click(function(event){
 
                 event.preventDefault();
-                
-                
+
+
                 var user = $('.user').val();
                 var pwd = $('.pwd').val();
                 var email = $('.email').val();
 
                 if (user == '' || pwd == '' || email == '') {
-                    
+
                     error_handler('please fill all inputs provided.....');
 
                 }else{
@@ -81,17 +94,17 @@
                         dataType: 'text',
                         beforeSend: function(){
 
-                            $('#reg_btn').val('verifying.......');
+                            $('#reg_btn').val('Verifying.......');
                             $('#reg_btn').attr('disabled', 'disabled');
-                        }, 
+                        },
 
                         success: function(data) {
-                            
-                            $('#reg_btn').val('verify');
+
+                            $('#reg_btn').val('Verify');
                             $('#reg_btn').attr('disabled', false);
 
                             if (data == 'verified') {
-                                
+
                                 window.location.assign("pupil_online_exam_login_form.php");
                             }else{
 
@@ -103,20 +116,10 @@
                 }
 
             })
-            
+
         })
 
 
     </script>
-
-
-
-
-
-
-
-
-
-
 </body>
 </html>

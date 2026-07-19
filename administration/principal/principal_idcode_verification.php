677c09d2f95ac0f37deece905063f12dedbf47cd
Yusuf Iysah  | UX Engineer <yusufiysah@gmail.com>
2026-07-19 13:42:32 +0100

    Refactor authentication styles and update footer text across principal pages

--- administration/principal/principal_idcode_verification.php
+++ administration/principal/principal_idcode_verification.php
@@ -16,13 +16,13 @@
 
 
     if (isset($_POST['submit'])) {
-        
+
         $id_code = mysqli_real_escape_string($conn, $_POST['id_code']);
 
         $principal_email = $_SESSION['principal_login_email'];
 
-        
-        
+
+
         $query = "SELECT * FROM principal_registration_table WHERE email = '$principal_email'";
 
         $query_run = mysqli_query($conn, $query);
@@ -30,7 +30,7 @@
         $num = mysqli_num_rows($query_run);
 
         if ($num > 0) {
-            
+
             $row = mysqli_fetch_array($query_run);
 
             $id_code_from_database = $row['id_code'];
@@ -38,7 +38,7 @@
            if ($id_code_from_database == $id_code) {
 
                 $_SESSION['principal_id_code'] = $row['id_code'];
-                
+
                 header("location: principal_home.php");
 
            }else{
@@ -58,19 +58,40 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>principal id_code verification</title>
-    <link rel="stylesheet" href="css/principal_auth_css.css">
+    <link rel="stylesheet" href="../css/auth_css.css?v=1">
 </head>
 <body>
-    <div id="form_container">
-        <div class="form_element">
-            <form action="principal_idcode_verification.php" method="POST">
-                <h2>principal id code verification</h2>
-                <input type="text" id="id_code" placeholder="Enter code from ur email" required name="id_code">
+    <main class="page">
+        <div class="card">
+            <div class="icon_badge">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+                    <polyline points="9 12 11 14 15 10"></polyline>
+                </svg>
+            </div>
 
-                
-                <input type="submit" name="submit" id="reg_btn" value="submit">
+            <h1>Verify your identity</h1>
+            <p class="sub">Enter the ID code sent to your email to continue.</p>
+
+            <form action="principal_idcode_verification.php" method="POST">
+                <div class="field">
+                    <label for="id_code">ID code</label>
+                    <input type="text" id="id_code" placeholder="Enter the code from your email" required name="id_code">
+                </div>
+
+                <input type="submit" name="submit" class="submit_btn" value="Verify">
             </form>
+
+            <a class="back_link" href="principal_login.php">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <line x1="19" y1="12" x2="5" y2="12"></line>
+                    <polyline points="12 19 5 12 12 5"></polyline>
+                </svg>
+                Back to login
+            </a>
         </div>
-    </div>
+
+        <p class="page_footer">Acadex &middot; by Zorfts Technologies Ltd</p>
+    </main>
 </body>
 </html>
--- administration/principal/principal_login.php
+++ administration/principal/principal_login.php
@@ -29,7 +29,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>principal login</title>
-    <link rel="stylesheet" href="css/principal_auth_css.css">
+    <link rel="stylesheet" href="../css/auth_css.css?v=1">
 </head>
 <body>
     <main class="page">

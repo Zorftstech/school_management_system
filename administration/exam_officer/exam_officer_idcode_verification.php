0ae29ea4c8f6de79c4ca640e9bd4238e2650c619
Yusuf Iysah  | UX Engineer <yusufiysah@gmail.com>
2026-07-19 13:45:39 +0100

    Redesign exam officer authentication pages with improved layout, unified CSS, and enhanced user feedback

--- administration/exam_officer/exam_officer_idcode_verification.php
+++ administration/exam_officer/exam_officer_idcode_verification.php
@@ -2,11 +2,11 @@
     session_start();
 
     if (isset($_SESSION['exam_officer_id_code'])) {
-        
+
         header("location: exam_officer_home.php");
     }
-    
-    
+
+
 ?>
 
 
@@ -17,27 +17,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>exam officer id_code verification</title>
-    <link rel="stylesheet" href="../admin_officer/css/admin_officer_login_css.css">
+    <link rel="stylesheet" href="../css/auth_css.css?v=1">
     <script src="../../javascript/jquery.js"></script>
 </head>
 <body>
-    <div id="form_container">
-        <div class="form_element">
-            <form action="principal_idcode_verification.php" method="POST" id="form">
+    <main class="page">
+        <div class="card">
+            <div class="icon_badge">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+                    <polyline points="9 12 11 14 15 10"></polyline>
+                </svg>
+            </div>
 
-                <div class="error">
-                    <p id="error"></p>
+            <h1>Verify your identity</h1>
+            <p class="sub">Enter the ID code sent to your email to continue.</p>
+
+            <p class="js_alert" id="error"></p>
+
+            <form method="POST" id="form">
+                <div class="field">
+                    <label for="id_code">ID code</label>
+                    <input type="text" id="id_code" placeholder="Enter the code from your email" required name="id_code">
                 </div>
 
-                <h2>exam officer id code verification</h2>
-
-                <input type="text" id="id_code" placeholder="Enter code from ur email" required name="id_code">
-
-                
-                <input type="submit" name="submit" id="reg_btn" value="submit">
+                <input type="submit" name="submit" id="reg_btn" class="submit_btn" value="Verify">
             </form>
+
+            <a class="back_link" href="exam_officer_login.php">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <line x1="19" y1="12" x2="5" y2="12"></line>
+                    <polyline points="12 19 5 12 12 5"></polyline>
+                </svg>
+                Back to login
+            </a>
         </div>
-    </div>
+
+        <p class="page_footer">Acadex &middot; by Zorfts Technologies Ltd</p>
+    </main>
 
 
 
@@ -61,18 +78,18 @@
 
 
         //  verifying admin officer email before login...........
-        
+
         $('#reg_btn').click(function(event) {
 
             event.preventDefault();
-            
+
             var id_code = $('#id_code').val();
-            
+
             if (id_code == '') {
-                
+
                 error_handler('please fill the space below........');
 
-                
+
 
             }else{
 
@@ -82,17 +99,17 @@
                     method: 'POST',
                     dataType: 'text',
                     beforeSend: function(){
-                        $('#reg_btn').val('varifying......');
-                        $('#reg_btn').attr('disabled', 'disabled'); 
+                        $('#reg_btn').val('Verifying......');
+                        $('#reg_btn').attr('disabled', 'disabled');
                     },
 
                     success: function(data){
-                        
-                        $('#reg_btn').val('varify');
-                        $('#reg_btn').attr('disabled', false); 
+
+                        $('#reg_btn').val('Verify');
+                        $('#reg_btn').attr('disabled', false);
 
                         if (data == 'verify') {
-                            
+
                             window.location.assign("exam_officer_home.php");
 
                         }else{
